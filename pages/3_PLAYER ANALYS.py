86c6dd087b2b906f99ed58cc03e1d79b8b31e7d3
gpain1999 <guillaume.pain9@gmail.com>
2024-11-18 20:35:44 +0100

    add graph

--- pages/3_PLAYER ANALYS.py
+++ pages/3_PLAYER ANALYS.py
@@ -11,13 +11,15 @@ data_dir = os.path.join(os.path.dirname(__file__), '../datas')
 sys.path.append(os.path.join(os.path.dirname(__file__), '../fonctions'))
 images_dir = os.path.join(os.path.dirname(__file__), '..', 'images')  # Path to the images directory
 
-
 import fonctions as f
 
 image_path = f"images/{competition}.png"  # Chemin vers l'image
 
 players = pd.read_csv(os.path.join(data_dir, f"{competition}_idplayers_{season}.csv"))
+gs = pd.read_csv(os.path.join(data_dir, f"{competition}_gs_{season}.csv"))
 
+gs = gs[["Round","local.club.code","local.score","road.score","road.club.code"]]
+gs.columns = ["ROUND","LOCAL","SL","SR","ROAD"]
 # Charger les données
 df = f.calcul_per2(data_dir, season, competition)
 df.insert(6, "I_PER", df["PER"] / df["TIME_ON"] ** 0.5)
@@ -27,12 +29,8 @@ df = df[['ROUND', 'NB_GAME', 'TEAM', 'OPPONENT', 'HOME', 'WIN', 'NUMBER', 'PLAYE
          'TIME_ON', "I_PER", 'PER', 'PM_ON', 'PTS', 'DR', 'OR', 'TR', 'AS', 'ST', 'CO',
          '1_R', '1_T', '2_R', '2_T', '3_R', '3_T', 'TO', 'FP', 'CF', 'NCF']]
 
-
 if os.path.exists(image_path):
-    # Charger l'image
     img = Image.open(image_path)
-
-    # Ajouter du CSS pour positionner l'image en haut à droite
     st.markdown(
         """
         <style>
@@ -46,8 +44,6 @@ if os.path.exists(image_path):
         """,
         unsafe_allow_html=True
     )
-
-    # Insérer l'image en utilisant HTML
     st.markdown(
         f"""
         <div class="top-right-image">
@@ -59,10 +55,7 @@ if os.path.exists(image_path):
 else:
     st.warning(f"L'image {image_path} est introuvable. Veuillez vérifier le chemin.")
 
-
-# Titre de l'application
-st.title("Diagramme : PER  et Minutes ")
-
+st.title("Analyse de joueur ")
 
 # Remplir les trous dans ROUND
 min_round, max_round = df["ROUND"].min(), df["ROUND"].max()
@@ -74,10 +67,9 @@ selected_range = st.sidebar.slider(
     "Sélectionnez une plage de ROUND :",
     min_value=min_round,
     max_value=max_round,
-    value=(min_round, max_round),  # Valeurs initiales : tout afficher
+    value=(min_round, max_round),
     step=1
 )
-
 
 # Filtrage dynamique des équipes
 CODETEAM = st.sidebar.selectbox("Équipe Sélectionnée", options=df["TEAM"].unique() if not df.empty else [])
@@ -90,64 +82,69 @@ else:
 
 selected_players = st.sidebar.selectbox("Joueur Sélectionné", options=available_players)
 
+selected_stats = st.sidebar.selectbox("Stat Sélectionné", options=["PER","I_PER","PTS","TR","AS"])
+
 
 filtered_df = f.get_aggregated_data(
-    df = df, min_round = selected_range[0], max_round = selected_range[1],
+    df=df, min_round=selected_range[0], max_round=selected_range[1],
     selected_teams=[CODETEAM],
     selected_opponents=[],
-    selected_fields=["ROUND","PLAYER","TEAM"],
+    selected_fields=["ROUND", "PLAYER", "TEAM"],
     selected_players=[selected_players],
     mode="CUMULATED",
     percent="PERCENT"
 )
+gs_filtered = gs[(gs["LOCAL"]==CODETEAM)|(gs["ROAD"]==CODETEAM)].reset_index(drop = True)
+gs_filtered['WIN'] = gs_filtered.apply(lambda row: 'YES' if 
+                     (row['LOCAL'] == CODETEAM and row['SL'] > row['SR']) or 
+                     (row['ROAD'] == CODETEAM and row['SL'] < row['SR']) 
+                     else 'NO', axis=1)
+
 
 NAME_PLAYER = filtered_df["PLAYER"].to_list()[0]
 NUMBER_PLAYER = filtered_df["#"].to_list()[0]
 TEAM_PLAYER = filtered_df["TEAM"].to_list()[0]
-PLAYER_ID = players[(players["CODETEAM"]==TEAM_PLAYER)&(players["PLAYER"]==NAME_PLAYER)]["PLAYER_ID"].to_list()[0]
+PLAYER_ID = players[(players["CODETEAM"] == TEAM_PLAYER) & (players["PLAYER"] == NAME_PLAYER)]["PLAYER_ID"].to_list()[0]
 
-filtered_df = filtered_df.drop(columns = ["NB_GAME","PLAYER","#","TEAM"] )
+filtered_df = filtered_df.drop(columns=["NB_GAME", "PLAYER", "#", "TEAM"])
 
 # Création du graphique avec Plotly Graph Objects
 fig = go.Figure()
 
-# Ajouter les barres pour PER avec des couleurs basées sur WIN
 for i, row in filtered_df.iterrows():
     color = "green" if row["WIN"] == "YES" else "red"
     fig.add_trace(go.Bar(
         x=[row["ROUND"]],
-        y=[row["PER"]],
+        y=[row[selected_stats]],
         marker_color=color,
         yaxis="y1",
-        showlegend=False  # Désactiver la légende pour les barres
+        showlegend=False
     ))
 
-# Ajouter la courbe pour Minutes
 fig.add_trace(go.Scatter(
     x=filtered_df["ROUND"],
     y=filtered_df["TIME_ON"],
     mode="lines+markers",
-    line=dict(color='blue', width=2),  # Courbe bleue
-    marker=dict(size=8, symbol="circle", color="blue"),  # Points bleus
+    line=dict(color='blue', width=2),
+    marker=dict(size=8, symbol="circle", color="blue"),
     yaxis="y2",
-    showlegend=False  # Désactiver la légende pour la courbe
+    showlegend=False
 ))
 
-# Configuration des axes
 fig.update_layout(
-    title=f'#{NUMBER_PLAYER} {NAME_PLAYER} ({TEAM_PLAYER}) : GRAPH',
+    title=f'#{NUMBER_PLAYER} {NAME_PLAYER} ({TEAM_PLAYER}) : {selected_stats}  et Minutes',
     xaxis=dict(
         title="ROUND",
-        showgrid=False,  # Désactiver la grille sur l'axe X
-        tickmode='linear',  # Garder des ticks réguliers
-        tick0=min_round,  # Aligner sur le premier ROUND
+        showgrid=False,
+        tickmode='linear',
+        tick0=min_round,
     ),
     yaxis=dict(
-        title="PER",
+        title=selected_stats,
         titlefont=dict(color="grey"),
         tickfont=dict(color="grey"),
-        showgrid=True,  # Grille légère pour Y1
-        gridcolor="rgba(200,200,200,0.3)",  # Couleur claire pour la grille
+        showgrid=True,
+        gridcolor="rgba(200,200,200,0.3)",
     ),
     yaxis2=dict(
         title="Minutes",
@@ -155,36 +152,92 @@ fig.update_layout(
         tickfont=dict(color="grey"),
         overlaying="y",
         side="right",
-        showgrid=False  # Pas de grille pour Y2
+        showgrid=False
     ),
-    margin=dict(l=50, r=50, t=50, b=50),  # Marges autour du graphique
+    margin=dict(l=50, r=50, t=50, b=50),
 )
 
-# Chemin vers la photo du joueur
 player_image_path = os.path.join(images_dir, f"{competition}_{season}_players/{TEAM_PLAYER}_{PLAYER_ID}.png")
 team_logo_path = os.path.join(images_dir, f"{competition}_{season}_teams/{TEAM_PLAYER}.png")
 
-# Mise en page : Ajouter la photo du joueur à gauche
-col1, col2, col3 = st.columns([1, 3, 2])  # Ajuster les colonnes pour inclure une photo à gauche
+col1, col2, col3 = st.columns([1, 3, 3])
 
 with col1:
-    # Afficher la photo du joueur si elle existe
     if os.path.exists(player_image_path):
         st.image(player_image_path, caption=f"#{NUMBER_PLAYER} {NAME_PLAYER}", width=150)
     else:
         st.warning(f"Image introuvable pour le joueur : {NAME_PLAYER}")
-    
-    # Afficher le logo du club si le fichier existe
+
     if os.path.exists(team_logo_path):
-        st.image(team_logo_path, caption=f"Équipe : {TEAM_PLAYER}", width=100)
+        st.image(team_logo_path, caption=f"Équipe : {TEAM_PLAYER}", width=150)
     else:
         st.warning(f"Logo introuvable pour l'équipe : {TEAM_PLAYER}")
 
 with col2:
-    # Afficher le graphique
     st.plotly_chart(fig, use_container_width=True)
 
+
+
+
 with col3:
-    # Afficher les données filtrées
-    st.header("Données filtrées")
-    st.dataframe(filtered_df)
+    # Création des boxplots avec Plotly Graph Objects
+    box_fig = go.Figure()
+
+    # Boxplot global pour PER
+    box_fig.add_trace(go.Box(
+        y=filtered_df[selected_stats],
+        name="Global",
+        marker_color="blue"
+    ))
+
+    # Boxplots pour chaque modalité de WIN
+    for win_status in filtered_df["WIN"].unique():
+        box_fig.add_trace(go.Box(
+            y=filtered_df[filtered_df["WIN"] == win_status][selected_stats],
+            name=f"WIN = {win_status}",
+            marker_color="green" if win_status == "YES" else "red"
+        ))
+
+    # Configuration du layout des boxplots
+    box_fig.update_layout(
+        title=f"Distribution de {selected_stats} (Global et par resultat)",
+        yaxis=dict(
+            title=selected_stats,
+            showgrid=True
+        ),
+        xaxis=dict(
+            title="Catégories",
+            showgrid=False
+        ),
+        margin=dict(l=50, r=50, t=50, b=50)
+    )
+
+    # Affichage des boxplots
+    st.header(f"Boxplots de {selected_stats}")
+    st.plotly_chart(box_fig, use_container_width=True)
+
+# Fonction pour appliquer un style basé sur la colonne WIN
+def highlight_win(row):
+    # Appliquer vert si WIN est "YES", rouge sinon, à toutes les colonnes
+    color = 'background-color: green; color: white;' if row["WIN"] == "YES" else 'background-color: red; color: white;'
+    return [color for _ in row.index]
+
+
+col1, col2 = st.columns([4, 1])
+
+
+with col1:
+
+    # Appliquer le style sur le DataFrame
+    styled_df = filtered_df.style.apply(highlight_win, axis=1).format(precision=1)  # Limiter à 1 décimales
+
+    # Afficher le tableau stylé dans Streamlit
+    st.header("Stats Joueur")
+    st.dataframe(styled_df, use_container_width=True)
+
+with col2:
+
+    gs_styled_df = gs_filtered.style.apply(highlight_win, axis=1)
+    st.header("Resultat Equipe")
+    st.dataframe(gs_styled_df)
+
