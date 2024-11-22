94babe2aaff3ec553e0be85e20c5428a99c0afef
gpain1999 <guillaume.pain9@gmail.com>
2024-11-22 14:04:38 +0100

    test logo

--- test_lancement_V2.py
+++ test_lancement_V2.py
@@ -5,16 +5,30 @@ st.set_page_config(page_title="Euroleague Data", layout="wide")
 competition = "euroleague"
 
 image_path = f"images/{competition}.png"  # Chemin vers l'image
-
+image_gp = f"images/logo_gp.png"
 
 try:
+    # Charger la première image
     image = Image.open(image_path)
-    # Redimensionner l'image (par exemple, largeur de 300 pixels)
-    max_width = 600
-    image = image.resize((max_width, int(image.height * (max_width / image.width))))
+    # Redimensionner la première image (par exemple, largeur de 600 pixels)
+    max_width1 = 600
+    image = image.resize((max_width1, int(image.height * (max_width1 / image.width))))
 
-    # Afficher l'image redimensionnée
-    st.image(image, caption=f"Rapport pour {competition}")
+    # Charger la deuxième image
+    image2 = Image.open(image_gp)
+    # Redimensionner la deuxième image (par exemple, largeur de 100 pixels)
+    max_width2 = 100
+    image2 = image2.resize((max_width2, int(image2.height * (max_width2 / image2.width))))
+
+    # Afficher les images côte à côte dans deux colonnes
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.image(image, caption=f"Rapport pour {competition}")
+
+    with col2:
+        st.image(image2, caption=f"Rapport pour {competition}")
+
 except FileNotFoundError:
     st.warning(f"L'image pour {competition} est introuvable à l'emplacement : {image_path}") 
 
