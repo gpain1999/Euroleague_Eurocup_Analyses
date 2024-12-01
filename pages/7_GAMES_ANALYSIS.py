23018ec1ed80414798a9a293d753639c390b1244
gpain1999 <guillaume.pain9@gmail.com>
2024-12-01 12:18:42 +0100

    game analysis

--- pages/7_GAMES_ANALYSIS.py
+++ pages/7_GAMES_ANALYSIS.py
@@ -58,6 +58,7 @@ gs = pd.read_csv(f"datas/{competition}_gs_{season}.csv")[["Gamecode", "Round", "
 gs.columns = ["GAMECODE", "ROUND", "LOCAL_TEAM", "LOCAL_SCORE", "ROAD_TEAM", "ROAD_SCORE"]
 gs["GAME"] = gs.apply(lambda row: f"R{row['ROUND']} : {row['LOCAL_TEAM']} - {row['ROAD_TEAM']}", axis=1)
 
+players = pd.read_csv(os.path.join(data_dir, f"{competition}_idplayers_{season}.csv"))
 
 ######################### PARAM
 st.sidebar.header("SETTINGS")
@@ -94,6 +95,11 @@ team_road = match.group(3)
 local_logo_path = os.path.join(images_dir, f"{competition}_{season}_teams/{team_local}.png")
 road_logo_path = os.path.join(images_dir, f"{competition}_{season}_teams/{team_road}.png")
 
+############################### DATA ###################################################
+
+periode,cumul = f.team_evol_score(team_local,r,r,data_dir,competition,season,type = "MEAN")
+
+
 player_stat = f.get_aggregated_data(
     df=df, min_round=r, max_round=r,
     selected_teams=[team_local,team_road],
@@ -108,10 +114,93 @@ player_stat = player_stat.sort_values(by = "TIME_ON",ascending = False)
 local_player_stat = player_stat[player_stat["TEAM"]==team_local]
 road_player_stat = player_stat[player_stat["TEAM"]==team_road]
 
-############################### DATA ###################################################
+### MVP ###
 
-periode,cumul = f.team_evol_score(team_local,r,r,data_dir,competition,season,type = "MEAN")
+mvp_data = player_stat[player_stat["WIN"]=="YES"].sort_values(by = ["I_PER","TIME_ON"],ascending = [False,True]).reset_index(drop = True)
+NAME_MVP = mvp_data["PLAYER"].to_list()[0]
+NUMBER_MVP = mvp_data["#"].to_list()[0]
+TEAM_MVP = mvp_data["TEAM"].to_list()[0]
+MVP_ID = players[(players["CODETEAM"] == TEAM_MVP) & (players["PLAYER"] == NAME_MVP)]["PLAYER_ID"].to_list()[0]
+mvp_image_path = os.path.join(images_dir, f"{competition}_{season}_players/{TEAM_MVP}_{MVP_ID}.png")
 
+BL_data = player_stat[player_stat["WIN"]=="NO"].sort_values(by = ["I_PER","TIME_ON"],ascending = [False,True]).reset_index(drop = True)
+NAME_BL = BL_data["PLAYER"].to_list()[0]
+NUMBER_BL = BL_data["#"].to_list()[0]
+TEAM_BL = BL_data["TEAM"].to_list()[0]
+BL_ID = players[(players["CODETEAM"] == TEAM_BL) & (players["PLAYER"] == NAME_BL)]["PLAYER_ID"].to_list()[0]
+BL_image_path = os.path.join(images_dir, f"{competition}_{season}_players/{TEAM_BL}_{BL_ID}.png")
+
+### PTS ###
+
+PTS_data = player_stat.sort_values(by = ["PTS","TIME_ON"],ascending = [False,True]).reset_index(drop = True)
+NAME_PTS = PTS_data["PLAYER"].to_list()[0]
+NUMBER_PTS = PTS_data["#"].to_list()[0]
+TEAM_PTS = PTS_data["TEAM"].to_list()[0]
+PTS_ID = players[(players["CODETEAM"] == TEAM_PTS) & (players["PLAYER"] == NAME_PTS)]["PLAYER_ID"].to_list()[0]
+PTS_image_path = os.path.join(images_dir, f"{competition}_{season}_players/{TEAM_PTS}_{PTS_ID}.png")
+
+### PM_ON ###
+
+PM_ON_data = player_stat.sort_values(by = ["PM_ON","TIME_ON"],ascending = [False,True]).reset_index(drop = True)
+NAME_PM_ON = PM_ON_data["PLAYER"].to_list()[0]
+NUMBER_PM_ON = PM_ON_data["#"].to_list()[0]
+TEAM_PM_ON = PM_ON_data["TEAM"].to_list()[0]
+PM_ON_ID = players[(players["CODETEAM"] == TEAM_PM_ON) & (players["PLAYER"] == NAME_PM_ON)]["PLAYER_ID"].to_list()[0]
+PM_ON_image_path = os.path.join(images_dir, f"{competition}_{season}_players/{TEAM_PM_ON}_{PM_ON_ID}.png")
+
+### DR ###
+
+DR_data = player_stat.sort_values(by = ["DR","TIME_ON"],ascending = [False,True]).reset_index(drop = True)
+NAME_DR = DR_data["PLAYER"].to_list()[0]
+NUMBER_DR = DR_data["#"].to_list()[0]
+TEAM_DR = DR_data["TEAM"].to_list()[0]
+DR_ID = players[(players["CODETEAM"] == TEAM_DR) & (players["PLAYER"] == NAME_DR)]["PLAYER_ID"].to_list()[0]
+DR_image_path = os.path.join(images_dir, f"{competition}_{season}_players/{TEAM_DR}_{DR_ID}.png")
+
+### OR ###
+
+OR_data = player_stat.sort_values(by = ["OR","TIME_ON"],ascending = [False,True]).reset_index(drop = True)
+NAME_OR = OR_data["PLAYER"].to_list()[0]
+NUMBER_OR = OR_data["#"].to_list()[0]
+TEAM_OR = OR_data["TEAM"].to_list()[0]
+OR_ID = players[(players["CODETEAM"] == TEAM_OR) & (players["PLAYER"] == NAME_OR)]["PLAYER_ID"].to_list()[0]
+OR_image_path = os.path.join(images_dir, f"{competition}_{season}_players/{TEAM_OR}_{OR_ID}.png")
+
+### AS ###
+
+AS_data = player_stat.sort_values(by = ["AS","TIME_ON"],ascending = [False,True]).reset_index(drop = True)
+NAME_AS = AS_data["PLAYER"].to_list()[0]
+NUMBER_AS = AS_data["#"].to_list()[0]
+TEAM_AS = AS_data["TEAM"].to_list()[0]
+AS_ID = players[(players["CODETEAM"] == TEAM_AS) & (players["PLAYER"] == NAME_AS)]["PLAYER_ID"].to_list()[0]
+AS_image_path = os.path.join(images_dir, f"{competition}_{season}_players/{TEAM_AS}_{AS_ID}.png")
+
+### TO ###
+
+TO_data = player_stat.sort_values(by = ["TO","TIME_ON"],ascending = [False,True]).reset_index(drop = True)
+NAME_TO = TO_data["PLAYER"].to_list()[0]
+NUMBER_TO = TO_data["#"].to_list()[0]
+TEAM_TO = TO_data["TEAM"].to_list()[0]
+TO_ID = players[(players["CODETEAM"] == TEAM_TO) & (players["PLAYER"] == NAME_TO)]["PLAYER_ID"].to_list()[0]
+TO_image_path = os.path.join(images_dir, f"{competition}_{season}_players/{TEAM_TO}_{TO_ID}.png")
+
+### ST ###
+
+ST_data = player_stat.sort_values(by = ["ST","TIME_ON"],ascending = [False,True]).reset_index(drop = True)
+NAME_ST = ST_data["PLAYER"].to_list()[0]
+NUMBER_ST = ST_data["#"].to_list()[0]
+TEAM_ST = ST_data["TEAM"].to_list()[0]
+ST_ID = players[(players["CODETEAM"] == TEAM_ST) & (players["PLAYER"] == NAME_ST)]["PLAYER_ID"].to_list()[0]
+ST_image_path = os.path.join(images_dir, f"{competition}_{season}_players/{TEAM_ST}_{ST_ID}.png")
+
+### CO ###
+
+CO_data = player_stat.sort_values(by = ["CO","TIME_ON"],ascending = [False,True]).reset_index(drop = True)
+NAME_CO = CO_data["PLAYER"].to_list()[0]
+NUMBER_CO = CO_data["#"].to_list()[0]
+TEAM_CO = CO_data["TEAM"].to_list()[0]
+CO_ID = players[(players["CODETEAM"] == TEAM_CO) & (players["PLAYER"] == NAME_CO)]["PLAYER_ID"].to_list()[0]
+CO_image_path = os.path.join(images_dir, f"{competition}_{season}_players/{TEAM_CO}_{CO_ID}.png")
 
 ############################ PRINT ########################################################
 
@@ -168,6 +257,8 @@ cola, colb = st.columns([1, 6])
 
 
 with cola :
+
+
     type_delta = st.selectbox("DELTA", options=["CUMULATED","PER PERIODE"], index=0)
 
     if type_delta == "PER PERIODE" :
@@ -294,6 +385,9 @@ with cola :
         fig2 = f.plot_semi_circular_chart(local_player_stat["OR"].sum()/(local_player_stat["OR"].sum() + road_player_stat["DR"].sum()),"OFF.",size=int(85*zoom), font_size=int(20*zoom))
         st.plotly_chart(fig2,use_container_width=True)
 
+        fig2 = f.plot_semi_circular_chart(local_player_stat["TR"].sum()/(local_player_stat["TR"].sum() + road_player_stat["TR"].sum()),"TOT",size=int(85*zoom), font_size=int(20*zoom))
+        st.plotly_chart(fig2,use_container_width=True)
+
     with reb_road :
         st.markdown(
             f'''
@@ -309,6 +403,8 @@ with cola :
         fig2 = f.plot_semi_circular_chart(road_player_stat["OR"].sum()/(road_player_stat["OR"].sum() + local_player_stat["DR"].sum()),"OFF.",size=int(85*zoom), font_size=int(20*zoom))
         st.plotly_chart(fig2,use_container_width=True)
 
+        fig2 = f.plot_semi_circular_chart(road_player_stat["TR"].sum()/(local_player_stat["TR"].sum() + road_player_stat["TR"].sum()),"TOT",size=int(85*zoom), font_size=int(20*zoom))
+        st.plotly_chart(fig2,use_container_width=True)
     st.markdown(
         f'''
         <p style="font-size:{int(35*zoom)}px; text-align: center; padding: 10pxs;">
@@ -348,6 +444,162 @@ with cola :
         fig2 = f.plot_semi_circular_chart(shot_T/(shot_T+TO),"BC", size=int(85*zoom), font_size=int(20*zoom))
         st.plotly_chart(fig2,use_container_width=True)
 with colb :
+
+    _,s1,s2,s3,s4,s5 = st.columns([2,1,1,1,1,1])
+
+    with s1 :
+        # Intégrer la couleur dans le markdown
+        st.markdown(
+            f'''
+            <p style="font-size:{int(30*zoom)}px; text-align: center;">
+                <b>MVP : {mvp_data["I_PER"].to_list()[0]} I_PER</b>
+            </p>
+            ''',
+            unsafe_allow_html=True
+        )
+
+        if os.path.exists(mvp_image_path):
+            st.image(mvp_image_path, caption=f"#{NUMBER_MVP} {NAME_MVP}", width=int(330*zoom))
+        else:
+            st.warning(f"Image introuvable pour le joueur : {NAME_MVP}")
+        # Intégrer la couleur dans le markdown
+        st.markdown(
+            f'''
+            <p style="font-size:{int(25*zoom)}px; text-align: center;">
+                <b>BEST LOSER : {BL_data["I_PER"].to_list()[0]} I_PER</b>
+            </p>
+            ''',
+            unsafe_allow_html=True
+        )
+
+        if os.path.exists(BL_image_path):
+            st.image(BL_image_path, caption=f"#{NUMBER_BL} {NAME_BL}", width=int(330*zoom))
+        else:
+            st.warning(f"Image introuvable pour le joueur : {NAME_BL}")
+
+    with s2 :
+        # Intégrer la couleur dans le markdown
+        st.markdown(
+            f'''
+            <p style="font-size:{int(30*zoom)}px; text-align: center;">
+                <b>MOST PTS : {PTS_data["PTS"].to_list()[0]} PTS</b>
+            </p>
+            ''',
+            unsafe_allow_html=True
+        )
+
+        if os.path.exists(PTS_image_path):
+            st.image(PTS_image_path, caption=f"#{NUMBER_PTS} {NAME_PTS}", width=int(330*zoom))
+        else:
+            st.warning(f"Image introuvable pour le joueur : {NAME_PTS}")
+
+        # Intégrer la couleur dans le markdown
+        st.markdown(
+            f'''
+            <p style="font-size:{int(30*zoom)}px; text-align: center;">
+                <b>MOST +/- : {int(PM_ON_data["PM_ON"].to_list()[0])}</b>
+            </p>
+            ''',
+            unsafe_allow_html=True
+        )
+
+        if os.path.exists(PM_ON_image_path):
+            st.image(PM_ON_image_path, caption=f"#{NUMBER_PM_ON} {NAME_PM_ON}", width=int(330*zoom))
+        else:
+            st.warning(f"Image introuvable pour le joueur : {NAME_PM_ON}")
+
+    with s3 :
+        # Intégrer la couleur dans le markdown
+        st.markdown(
+            f'''
+            <p style="font-size:{int(30*zoom)}px; text-align: center;">
+                <b>MOST DR : {DR_data["DR"].to_list()[0]} DR</b>
+            </p>
+            ''',
+            unsafe_allow_html=True
+        )
+
+        if os.path.exists(DR_image_path):
+            st.image(DR_image_path, caption=f"#{NUMBER_DR} {NAME_DR}", width=int(330*zoom))
+        else:
+            st.warning(f"Image introuvable pour le joueur : {NAME_DR}")
+
+        st.markdown(
+            f'''
+            <p style="font-size:{int(30*zoom)}px; text-align: center;">
+                <b>MOST OR : {OR_data["OR"].to_list()[0]} OR</b>
+            </p>
+            ''',
+            unsafe_allow_html=True
+        )
+
+        if os.path.exists(OR_image_path):
+            st.image(OR_image_path, caption=f"#{NUMBER_OR} {NAME_OR}", width=int(330*zoom))
+        else:
+            st.warning(f"Image introuvable pour le joueur : {NAME_OR}")
+            
+    with s4 :
+        # Intégrer la couleur dans le markdown
+        st.markdown(
+            f'''
+            <p style="font-size:{int(30*zoom)}px; text-align: center;">
+                <b>MOST AS : {AS_data["AS"].to_list()[0]} AS</b>
+            </p>
+            ''',
+            unsafe_allow_html=True
+        )
+
+        if os.path.exists(AS_image_path):
+            st.image(AS_image_path, caption=f"#{NUMBER_AS} {NAME_AS}", width=int(330*zoom))
+        else:
+            st.warning(f"Image introuvable pour le joueur : {NAME_AS}")
+
+        # Intégrer la couleur dans le markdown
+        st.markdown(
+            f'''
+            <p style="font-size:{int(30*zoom)}px; text-align: center;">
+                <b>MOST TO : {TO_data["TO"].to_list()[0]} TO</b>
+            </p>
+            ''',
+            unsafe_allow_html=True
+        )
+
+        if os.path.exists(TO_image_path):
+            st.image(TO_image_path, caption=f"#{NUMBER_TO} {NAME_TO}", width=int(330*zoom))
+        else:
+            st.warning(f"Image introuvable pour le joueur : {NAME_TO}")
+
+    with s5 :
+        # Intégrer la couleur dans le markdown
+        st.markdown(
+            f'''
+            <p style="font-size:{int(30*zoom)}px; text-align: center;">
+                <b>MOST ST : {ST_data["ST"].to_list()[0]} ST</b>
+            </p>
+            ''',
+            unsafe_allow_html=True
+        )
+
+        if os.path.exists(ST_image_path):
+            st.image(ST_image_path, caption=f"#{NUMBER_ST} {NAME_ST}", width=int(330*zoom))
+        else:
+            st.warning(f"Image introuvable pour le joueur : {NAME_ST}")
+
+        # Intégrer la couleur dans le markdown
+        st.markdown(
+            f'''
+            <p style="font-size:{int(30*zoom)}px; text-align: center;">
+                <b>MOST CO : {CO_data["CO"].to_list()[0]} CO</b>
+            </p>
+            ''',
+            unsafe_allow_html=True
+        )
+
+        if os.path.exists(CO_image_path):
+            st.image(CO_image_path, caption=f"#{NUMBER_CO} {NAME_CO}", width=int(330*zoom))
+        else:
+            st.warning(f"Image introuvable pour le joueur : {NAME_CO}")
+
     t = st.selectbox("TEAM", options=[team_local,team_road], index=0)
     player_stat = player_stat[player_stat["TEAM"]==t].drop(columns = ["TEAM","ROUND","NB_GAME","OPPONENT","HOME","WIN"])
     player_stat = player_stat.sort_values(by = "TIME_ON",ascending = False)
