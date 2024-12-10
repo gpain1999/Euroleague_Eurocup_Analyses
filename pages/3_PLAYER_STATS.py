66fe64ec89258ffa6f5cdc82e6d0a5341a3916ad
gpain1999 <80475152+gpain1999@users.noreply.github.com>
2024-12-10 14:44:50 +0100

    Update 3_PLAYER_STATS.py

--- pages/3_PLAYER_STATS.py
+++ pages/3_PLAYER_STATS.py
@@ -771,8 +771,8 @@ with col1:
         
         st.markdown(
         f'''
-        <p style="font-size:{int(40*zoom)}px; text-align: center; background-color: {local_c1} ;color: {local_c2}; padding: 2px; border-radius: 5px;outline: 3px solid {local_c2};">
-            <b>{round((df_resultat["2_R"].sum()*2 + df_resultat["3_R"].sum()*3)/(df_resultat["2_T"].sum() + df_resultat["3_T"].sum()),2)}</b>
+        <p style="font-size:{int(30*zoom)}px; text-align: center; background-color: {local_c1} ;color: {local_c2}; padding: 2px; border-radius: 5px;outline: 3px solid {local_c2};">
+            <b>{round((df_resultat["2_R"].sum()*2 + df_resultat["3_R"].sum()*3)/(df_resultat["2_T"].sum() + df_resultat["3_T"].sum()),2)} PTS/SHOOTS</b>
         </p>
         ''',
         unsafe_allow_html=True
