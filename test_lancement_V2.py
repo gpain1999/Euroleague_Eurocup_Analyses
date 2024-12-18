73bea4fbe713bd1e02b0125685c4f12d7839080d
gpain1999 <guillaume.pain9@gmail.com>
2024-12-18 09:15:48 +0100

    all

--- test_lancement_V2.py
+++ test_lancement_V2.py
@@ -5,7 +5,6 @@ st.set_page_config(page_title="Euroleague Data", layout="wide")
 competition = "euroleague"
 
 image_path = f"images/{competition}.png"  # Chemin vers l'image
-image_gp = f"images/logo_gp.png"
 
 try:
     # Charger la première image
@@ -14,11 +13,6 @@ try:
     max_width1 = 600
     image = image.resize((max_width1, int(image.height * (max_width1 / image.width))))
 
-    # # Charger la deuxième image
-    # image2 = Image.open(image_gp)
-    # # Redimensionner la deuxième image (par exemple, largeur de 100 pixels)
-    # max_width2 = 100
-    # image2 = image2.resize((max_width2, int(image2.height * (max_width2 / image2.width))))
 
     # Afficher les images côte à côte dans deux colonnes
     col1, col2 = st.columns(2)
@@ -26,11 +20,8 @@ try:
     with col1:
         st.image(image, caption=f"Rapport pour {competition}")
 
-    # with col2:
-        # st.image(image2, caption=f"Rapport pour {competition}")
 
 except FileNotFoundError:
     st.warning(f"L'image pour {competition} est introuvable à l'emplacement : {image_path}") 
 
 st.title("Application Euroleague - by gpain1999")
-#st.sidebar.success("Choisissez une page ci-dessus.")
