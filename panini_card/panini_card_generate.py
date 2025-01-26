07b88b3c8f511d5d70ca203d092998ea58fc92ff
gpain1999 <guillaume.pain9@gmail.com>
2025-01-26 21:12:24 +0100

    full

--- panini_card/panini_card_generate.py
+++ panini_card/panini_card_generate.py
@@ -9,7 +9,7 @@ height = round(850 * 88/63) - 60
 
 font_path_bold = "police_ecriture/GothamBoldItalic.ttf" 
 text_color = "black"
-border_size = 30
+border_size = 80
 dossier_path = f'images/{competition}_{season}_players'  # Remplacez par le chemin de votre dossier
 ct = pd.read_csv("panini_card/color_team.csv")
 if competition == "euroleague" :
@@ -55,57 +55,33 @@ for index, row in data.iterrows():
 
         panini_image = Image.new("RGB", (width, height), background_color)
 
-        #PLAYER
-        player_image_path = f"images/{competition}_{season}_players/{team}_{id_players}.png"
-        player_image = Image.open(player_image_path)
-        player_image = player_image.resize((815,1087), Image.Resampling.LANCZOS)
-        
-        player_image = player_image.convert("RGBA")
-        _bg = Image.new("RGBA", player_image.size, background_color)
-        player_image_with_bg = Image.alpha_composite(_bg, player_image)
-        player_image_with_bg = player_image_with_bg.convert("RGB")
-        panini_image.paste(player_image_with_bg, (round((width - 815)/2), 50))
-
-
-
-        #TEAM
-        team_image_path = f"images/{competition}_{season}_teams/{team}.png"
-        team_image = Image.open(team_image_path)
-        # Redimensionner l'image
-        new_size = (int(team_image.width * 1), int(team_image.height * 1))
-        team_image = team_image.resize(new_size, Image.Resampling.LANCZOS)
-        # Convertir en RGBA pour gérer la transparence
-        team_image = team_image.convert("RGBA")
-        # Coller l'image avec fond tout en préservant la transparence
-        panini_image.paste(team_image, (40, 50), mask=team_image)
-
-
-        #COMPETITON
-        competition_image_path = f"images/{competition}_little.png"
-        competition_image = Image.open(competition_image_path)
-        new_size = (int(competition_image.width * 0.25), int(competition_image.height * 0.25))
-        competition_image = competition_image.resize(new_size, Image.Resampling.LANCZOS)
-        competition_image = competition_image.convert("RGBA")
-
-        panini_image.paste(competition_image, (width - 200, 60), mask=competition_image)
-
-        #PAYS
-        # pays_image_path = f"images/flags/{pays}.png"
-        # pays_image = Image.open(pays_image_path)
-        # new_size = (int(pays_image.width * 0.2), int(pays_image.height * 0.2))
-        # pays_image = pays_image.resize(new_size, Image.Resampling.LANCZOS)
-        # pays_image = pays_image.convert("RGBA")
-
-        #panini_image.paste(pays_image, (width - 200, 200), mask=pays_image)
+        # Définir les coordonnées du bandeau haut
+        x0, y0 = 0,  0
+        x1, y1 = width, 200
 
         draw = ImageDraw.Draw(panini_image)
 
-        # Définir les coordonnées du bandeau
+        # Dessiner le bandeau
+        draw.rectangle([x0, y0, x1, y1], fill="#E59D63")
+
+        #PLAYER
+        player_image_path = f"images/{competition}_{season}_players/{team}_{id_players}.png"
+        player_image = Image.open(player_image_path)
+        player_image = player_image.resize((770,1027), Image.Resampling.LANCZOS)
+        
+        player_image = player_image.convert("RGBA")
+
+
+        panini_image.paste(player_image, (10, 50), mask=player_image)
+
+
+
+        # Définir les coordonnées du bandeau bas
         x0, y0 = 0, height - 200 + 50
         x1, y1 = width, height + 50
 
         # Dessiner le bandeau
-        draw.rectangle([x0, y0, x1, y1 + 100], fill=background_color)
+        draw.rectangle([x0, y0, x1, y1 + 100], fill="#E59D63")
 
         #NOM DU JOUEUR
         font_size = 85
@@ -128,12 +104,40 @@ for index, row in data.iterrows():
         text_height = bbox[3] - bbox[1]  # Hauteur = y_max - y_min
         draw.text(((panini_image.width - text_width) // 2, 50), f"{season}-{season+1}", fill=text_color, font=ImageFont.truetype(font_path_bold, 35))
 
+
+
+        #TEAM
+        team_image_path = f"images/{competition}_{season}_teams/{team}.png"
+        team_image = Image.open(team_image_path)
+        # Redimensionner l'image
+        new_size = (160,160)
+        team_image = team_image.resize(new_size, Image.Resampling.LANCZOS)
+        # Convertir en RGBA pour gérer la transparence
+        team_image = team_image.convert("RGBA")
+        # Coller l'image avec fond tout en préservant la transparence
+        panini_image.paste(team_image, (40, 40), mask=team_image)
+
+
+        #COMPETITON
+        competition_image_path = f"images/{competition}_little.png"
+        competition_image = Image.open(competition_image_path)
+        new_size = (160,160)
+        competition_image = competition_image.resize(new_size, Image.Resampling.LANCZOS)
+        competition_image = competition_image.convert("RGBA")
+
+        panini_image.paste(competition_image, (width - 200, 40), mask=competition_image)
+
+
+
+
+
+
         # BORDURE
         # Créer l'image avec le contour
         color = ct.loc[ct["TEAM"] == team, "COLOR"].values
 
-        panini_image = ImageOps.expand(panini_image, border=int(border_size*1/3), fill=color[0])
+        panini_image = ImageOps.expand(panini_image, border=int(border_size*1/5), fill=color[0])
 
-        panini_image = ImageOps.expand(panini_image, border=int(border_size*2/3), fill=border_color)
+        panini_image = ImageOps.expand(panini_image, border=int(border_size*4/5), fill=border_color)
 
         panini_image.save(f"{folder_path}/{team}_card_{id_players}.png")
